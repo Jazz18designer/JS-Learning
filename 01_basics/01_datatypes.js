3e2cf45298d4775c145e7ebd4a18214832c6d6f5
Aakash Sharma <81143712+Jazz18designer@users.noreply.github.com>
2025-05-20 10:23:05 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using node.js, not browser
+
+// console.log(3
+//     +
+//     3) //readablity should be high
+
+// console.log("Aakash")
+
+
+let name = "Aakash"
+let age = 18
+let isLoggedIn = false
+
+
+
+/* --Primitive Data Types-- */
+// number => 2 to power 53
+// bigint =>
+// string =>
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
