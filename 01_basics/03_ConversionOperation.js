b665e072a50bc25335e0940f94077647b1c6a960
Aakash Sharma <81143712+Jazz18designer@users.noreply.github.com>
2025-05-27 08:43:26 +0000

    Conversion of datatypes

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,6 +1,30 @@
-let score = "33"
+let score = undefined
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let valueInNumber = Number(score)
+
+// console.log(typeof (valueInNumber))
+// console.log(valueInNumber)
+
+// "33"    =>  33
+// "33abc" => NAN
+// True    => 1
+// False   => 0
+
+
+let isLoggedIn = 0
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(BooleanIsLoggedIn)
+
+// 1 => True; 0 => Flase;
+// "" => false
+// "aakash" => true
+
+let age = 30
+
+let stringNumber = String(age)
+console.log(stringNumber)
+console.log(typeof (stringNumber))
